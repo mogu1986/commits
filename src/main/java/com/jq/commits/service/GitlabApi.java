45f960fa95799487216cd97b112fea8399f30f7b
gaowei <1qaz2wsx>
2021-01-14 23:38:51 +0800

    u

--- src/main/java/com/jq/commits/service/GitlabApi.java
+++ src/main/java/com/jq/commits/service/GitlabApi.java
@@ -4,6 +4,8 @@ import com.jq.commits.entity.Commit;
 import com.jq.commits.entity.Project;
 import org.gitlab4j.api.GitLabApi;
 import org.gitlab4j.api.GitLabApiException;
+
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -14,6 +16,16 @@ public class GitlabApi {
         GitLabApi gitLabApi = new GitLabApi("http://gitlab.top.mw", "N99pyaZem6sqbjvUPHSi");
         List<org.gitlab4j.api.models.Commit> commits = gitLabApi.getCommitsApi().getCommits(project.getId(), project.getBranch(), start, end);
         return null;
+    }
+
+    public static void main(String[] args) throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date start = sdf.parse("2021-01-01 00:00:00");
+        Date end = sdf.parse("2021-01-13 23:59:59");
+
+        GitLabApi gitLabApi = new GitLabApi("http://gitlab.top.mw", "N99pyaZem6sqbjvUPHSi");
+        List<org.gitlab4j.api.models.Commit> commits = gitLabApi.getCommitsApi().getCommits(273,"master", start, end);
+        System.out.println(commits.size());
     }
 
 }
