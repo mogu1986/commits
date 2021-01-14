7a196338dc53236587beaced7c40e974540e23bc
gaowei <1qaz2wsx>
2021-01-14 22:04:07 +0800

    Initial commit

---
+++ src/main/java/com/jq/commits/CommitsApplication.java
@@ -0,0 +1,13 @@
+package com.jq.commits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommitsApplication.class, args);
+    }
+
+}
