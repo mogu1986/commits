d433b3d78a2d979a2a6226ead907be15dc1b7097
gaowei <1qaz2wsx>
2021-01-14 22:46:36 +0800

    u

--- src/main/java/com/jq/commits/CommitsApplication.java
+++ src/main/java/com/jq/commits/CommitsApplication.java
@@ -2,12 +2,20 @@ package com.jq.commits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class CommitsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CommitsApplication.class, args);
+    }
+
+    @GetMapping("/index")
+    public String mm() {
+        return "index";
     }
 
 }
